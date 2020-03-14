6f23b525860b99fbf31cd0cb9cb81a9faa9474e4
AsyPredelieva <asy.predelieva@gmail.com>
2020-03-14 23:53:12 +0200

    Home page redesign

---
+++ src/components/Teaser.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="teaser">
+        <ul>
+            <li><strong>Quick and easy</strong> find out the actual menus.</li>
+            <li><strong>Fast and furious</strong> make your choice.</li>
+            <li>
+                <strong>Gently and precisely</strong> order your favourite meal.
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Teaser'
+}
+</script>
+
+<style scoped lang="scss">
+.teaser {
+    width: 100%;
+
+    ul {
+        display: flex;
+        margin-left: -15px;
+        margin-right: -15px;
+
+        li {
+            flex: 1 1 0px;
+            margin: 0 15px;
+            border: 1px solid rgba(33, 147, 208, 0.3);
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="home container">
-        <Intro />
+    <div class="home">
+        <Hero></Hero>
+        <div class="container">
+            <h2>The best time of the day is comming</h2>
+            <Teaser></Teaser>
+        </div>
     </div>
 </template>
 
 <script>
-import Intro from '../components/Intro'
+import Hero from '../components/Hero'
+import Teaser from '../components/Teaser'
 
 export default {
     name: 'Home',
     components: {
-        Intro
+        Hero,
+        Teaser
     }
 }
 </script>
