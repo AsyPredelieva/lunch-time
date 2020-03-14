c89cff1d92a60864e573f72c63548c6684043d19
AsyPredelieva <asy.predelieva@gmail.com>
2020-03-15 00:13:53 +0200

    add Teaser component

--- src/components/Teaser.vue
+++ src/components/Teaser.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="teaser">
         <ul>
-            <li><strong>Quick and easy</strong> find out the actual menus.</li>
-            <li><strong>Fast and furious</strong> make your choice.</li>
-            <li>
+            <li class="quick">
+                <strong>Quick and easy</strong> find out the actual menus.
+            </li>
+            <li class="easy">
+                <strong>Fast and furious</strong> make your choice.
+            </li>
+            <li class="gently">
                 <strong>Gently and precisely</strong> order your favourite meal.
             </li>
         </ul>
@@ -29,6 +33,28 @@ export default {
             flex: 1 1 0px;
             margin: 0 15px;
             border: 1px solid rgba(33, 147, 208, 0.3);
+            padding: 120px 20px 20px;
+
+            &.quick {
+                background: url(../assets/icon-quick-easy.png) center 20px
+                    no-repeat;
+                background-size: 85px;
+            }
+
+            &.easy {
+                background: url(../assets/icon-fast.png) center -50px no-repeat;
+                background-size: 155px;
+            }
+
+            &.gently {
+                background: url(../assets/icon-gentle.png) center 30px no-repeat;
+                background-size: 95px;
+            }
+
+            strong {
+                display: block;
+                font-size: 20px;
+            }
         }
     }
 }
