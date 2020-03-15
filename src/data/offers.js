060ef38dbc0a5cb878201cadaf41bb856c5a500a
AsyPredelieva <asy.predelieva@gmail.com>
2020-03-15 19:14:11 +0200

    list offers

--- src/data/offers.js
+++ src/data/offers.js
@@ -2,21 +2,25 @@ export const offers = [
     {
         id: 0,
         name: 'Kamenitza',
-        isActive: false
+        imgUrl:
+            'https://images.pexels.com/photos/769289/pexels-photo-769289.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
     },
     {
         id: 1,
         name: 'Riviera',
-        isActive: false
+        imgUrl:
+            'https://images.pexels.com/photos/709823/pexels-photo-709823.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     },
     {
         id: 2,
         name: 'Juzhen polah',
-        isActive: false
+        imgUrl:
+            'https://images.pexels.com/photos/3493579/pexels-photo-3493579.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     },
     {
         id: 3,
         name: 'Dolce Pizza',
-        isActive: false
+        imgUrl:
+            'https://images.pexels.com/photos/248428/pexels-photo-248428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     }
 ]
