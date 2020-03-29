f3f69e6fa655db32388f31b6c2ddd6b4c7c1da2f
AsyPredelieva <asy.predelieva@gmail.com>
2020-03-30 00:04:45 +0300

    NotFound page changes

--- src/components/core/NotFound.vue
+++ src/components/core/NotFound.vue
@@ -1,5 +1,7 @@
 <template>
-    <h2>Page not found</h2>
+    <div class="container">
+        <h2>Page not found</h2>
+    </div>
 </template>
 
 <script>
@@ -8,4 +10,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.container {
+    margin: 40px auto;
+}
+</style>
